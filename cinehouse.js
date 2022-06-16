d17cfb1be2ca6ab6d504ba4ce11a0c1212874f1f
Viviane de Lima <delimaviviane@yahoo.com.br>
2022-06-16 17:58:22 -0300

    first commit

---
+++ cinehouse.js
@@ -0,0 +1,37 @@
+let cinema = 'CineHouse'
+let catalogo = require ('./database/catalogo.json');
+console.log(catalogo)
+
+function adicionarFilme(codigo, titulo, duracao, atores, anoDeLancamento, emCartaz) {
+  const novoFilme = {
+    codigo:codigo,
+    titulo: titulo,
+    duracao: duracao,
+    atores: atores,
+    anoDeLancamento: anoDeLancamento,
+    emCartaz: emCartaz,
+    }
+
+  catalogo.filmes.push(novoFilme)
+}
+
+//adicionarFilme(3, "A Origem", 3, "Leonardo DiCaprio", 2010, true)
+//console.log(catalogo)
+
+
+function buscarFilme (codigo) {
+    for (let i=0; i<catalogo.length; i++) {
+        if (catalogo[i].codigo == codigo) {
+            return catalogo[i];
+        }
+    }
+    return undefined;
+}
+function alterarStatusEmCartaz(codigo) {
+    for (let i=0; i<catalogo.length; i++) {
+        if (catalogo[i].codigo == codigo) {
+            catalogo[i].emCartaz = !catalogo[i].emCartaz;
+        }
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+## Repositorio Privado - CineHouse
